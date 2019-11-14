9d0506a7a037a9fdc8807634e8d3f182a84eb6f1
Gustavo Saiani <gustavo@poe.ma>
2019-11-14 10:35:04 -0300

    Start CarouselButton

---
+++ src/tests/CarouselButton.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import CarouselButton from '../CarouselButton';
+
+configure({ adapter: new Adapter() });
+
+describe('CarouselButton', () => {
+  const text = 'Button text';
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
+  });
+
+  it('renders a <button>', () => {
+    expect(wrapper.type()).toBe('button');
+  });
+
+  it('passes `children` through to the <button>', () => {
+    expect(wrapper.prop('children')).toBe(text);
+  });
+});
