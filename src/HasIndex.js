a02ef7259490adc59b77d951902edd1573c9df83
Gustavo Saiani <gustavo@poe.ma>
2019-11-29 09:26:50 -0300

    Allow the index in HasIndex to be controlled

--- src/HasIndex.js
+++ src/HasIndex.js
@@ -1,29 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default (Component, indexPropName) =>
-  class ComponentWithIndex extends React.PureComponent {
-    static displayName =
-      `HasIndex(${Component.displayName || Component.name})`;
+const capitalize = word => `${word[0].toUpperCase()}${word.slice(1)}`; // (1)
 
-    state = {
-      index: 0,
+export default (Component, indexPropName) => {
+  const defaultIndexPropName = `default${capitalize(indexPropName)}`;
+
+  return class ComponentWithIndex extends React.PureComponent {
+    static displayName = `HasIndex(${Component.displayName ||
+      Component.name})`;
+
+    static propTypes = {
+      [indexPropName]: PropTypes.number, // (2)
+      [defaultIndexPropName]: PropTypes.number,
+      onIndexChange: PropTypes.func,
     };
 
+    static defaultProps = {
+      [defaultIndexPropName]: 0,
+    };
+
+    static getDerivedStateFromProps(props, state) { // (3)
+      if (
+        props[indexPropName] != null &&
+        props[indexPropName] !== state.index
+      ) {
+        return { index: props[indexPropName] };
+      }
+      return null;
+    }
+
+    constructor(props) { // (4)
+      super(props);
+
+      this.state = {
+        index: props[defaultIndexPropName],
+      };
+    }
+
     handleDecrement = upperBound => {
+      const { onIndexChange } = this.props;
       this.setState(({ index }) => {
         const newIndex = upperBound
           ? (index + upperBound - 1) % upperBound
           : index - 1;
-
+        if (onIndexChange) {
+          onIndexChange({ target: { value: newIndex } });
+        }
         return {
           index: newIndex,
         };
       });
-    }
+    };
 
     handleIncrement = upperBound => {
+      const { onIndexChange } = this.props;
       this.setState(({ index }) => {
         const newIndex = upperBound ? (index + 1) % upperBound : index + 1;
+        if (onIndexChange) {
+          onIndexChange({ target: { value: newIndex } });
+        }
         return {
           index: newIndex,
         };
@@ -31,12 +67,16 @@ export default (Component, indexPropName) =>
     };
 
     render() {
+      const {
+        [defaultIndexPropName]: _defaultIndexProp,
+        ...rest
+      } = this.props;
       const indexProps = {
         [indexPropName]: this.state.index,
         [`${indexPropName}Decrement`]: this.handleDecrement,
         [`${indexPropName}Increment`]: this.handleIncrement,
       };
-
-      return <Component {...this.props} {...indexProps} />;
+      return <Component {...rest} {...indexProps} />;
     }
   };
+};
--- src/tests/HasIndex.test.js
+++ src/tests/HasIndex.test.js
@@ -18,8 +18,37 @@ describe('HasIndex()', () => {
     wrapper = shallow(<MockComponentWithIndex />);
   });
 
-  it('has an initial `index` state of 0', () => {
+  it('has initial `index` state equal to the `defaultIndex` prop', () => { //(1)
     expect(wrapper.state('index')).toBe(0);
+    const wrapper2 = shallow(<MockComponentWithIndex defaultIndex={1} />);
+    expect(wrapper2.state('index')).toBe(1);
+  });
+
+  it('always has `index` state equal to the `index` prop', () => { //(2)
+    const wrapperWithInitialIndex = shallow(
+      <MockComponentWithIndex index={1} />
+    );
+    expect(wrapperWithInitialIndex.state('index')).toBe(1);
+    wrapper.setProps({ index: 2 });
+    expect(wrapper.state('index')).toBe(2);
+  });
+
+  it('allows `index` state to change if the `index` prop is unset', () => { //(3)
+    const wrapperWithInitialIndex = shallow(
+      <MockComponentWithIndex index={1} />
+    );
+    wrapperWithInitialIndex.setProps({ index: undefined });
+    wrapperWithInitialIndex.setState({ index: 3 });
+    expect(wrapperWithInitialIndex.state('index')).toBe(3);
+  });
+
+  it('calls `onIndexChange` on decrement/increment', () => { //(4)
+    const onIndexChange = jest.fn();
+    wrapper.setProps({ index: 0, onIndexChange });
+    wrapper.prop('indexDecrement')(3);
+    expect(onIndexChange).toHaveBeenCalledWith({ target: { value: 2 } });
+    wrapper.prop('indexIncrement')(3);
+    expect(onIndexChange).toHaveBeenCalledWith({ target: { value: 1 } });
   });
 
   it('passes the `index` state down as the `index` prop', () => {
@@ -34,7 +63,7 @@ describe('HasIndex()', () => {
     expect(wrapper.state('index')).toBe(2);
   });
 
-  it('has an `index` of 1 on increment', () => {
+  it('has an `index` state of 1 on increment', () => {
     wrapper.prop('indexIncrement')();
     expect(wrapper.state('index')).toBe(1);
   });
